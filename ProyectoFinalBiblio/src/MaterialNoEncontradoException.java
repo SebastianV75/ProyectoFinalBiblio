3df278a3e6e937fe726c87bc62bd0a5a0e303940
SebastianV75 <92833858+SebastianV75@users.noreply.github.com>
2025-05-26 17:38:35 -0600

    Add files via upload

---
+++ ProyectoFinalBiblio/src/MaterialNoEncontradoException.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Exception.java to edit this template
+ */
+
+/**
+ *
+ * @author sebas
+ */
+public class MaterialNoEncontradoException {
+
+   /**
+    * Creates a new instance of <code>MaterialNoEncontradoException</code>
+    * without detail message.
+    */
+   public MaterialNoEncontradoException() {
+   }
+
+   /**
+    * Constructs an instance of <code>MaterialNoEncontradoException</code> with
+    * the specified detail message.
+    *
+    * @param msg the detail message.
+    */
+   public MaterialNoEncontradoException(String msg) {
+      super(msg);
+   }
+}
